5703da8cc799ab65dae8347d8ef504549e3d436c
Roberth Strand <me@robstr.dev>
2024-10-10 12:34:58 +0200

    feat: github cli
    Added steps to install the GitHub CLI.
    
    Signed-off-by: Roberth Strand <me@robstr.dev>

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -23,6 +23,7 @@ sudo apt-get install apt-transport-https --yes
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 sudo apt-get update
 sudo apt-get install helm
+# 03.02. trivy
 echo "Installing trivy..."
 curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin
 echo "k8s tools installed successfully."
@@ -34,5 +35,14 @@ sudo gpg --no-default-keyring --keyring /etc/apt/keyrings/bruno.gpg --keyserver 
 
 echo "deb [signed-by=/etc/apt/keyrings/bruno.gpg] http://debian.usebruno.com/ bruno stable" | sudo tee /etc/apt/sources.list.d/bruno.list
 sudo apt update
-sudo apt install bruno
-echo "Bruno installed successfully."
+sudo apt install bruno && echo "Bruno installed successfully."
+
+# 05. gh cli
+echo "Installing gh cli..."
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y && echo "gh cli installed successfully."
