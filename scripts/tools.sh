d73ca0e313a2273fd597d0c37548c2a6fa5f0167
Vegard Eikenes <vegard.eikenes@banenor.no>
2024-10-22 13:49:28 +0200

    feat: add node and test dependencies to runners

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -28,6 +28,18 @@ echo "Installing trivy..."
 curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin
 echo "k8s tools installed successfully."
 
+curl -fsSL https://deb.nodesource.com/setup_22.x
+sudo apt-get update
+sudo apt-get install nodejs
+sudo apt-get install npm
+npx playwright install --with-deps
+npm install -D @playwright/test
+npm i create-playwright --quiet --with-deps
+npm install -g @usebruno/cli
+npm install -g dotenv-cli
+npm install -D typescript
+npm install --save @types/node
+
 # # 04. Bruno
 # echo "Installing Bruno..."
 # sudo mkdir -p /etc/apt/keyrings 
