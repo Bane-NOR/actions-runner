6571dca5203eb690ce967197c41b078d443838a2
Nicklas Risan Dahl <111047492+NicklasOL@users.noreply.github.com>
2026-04-08 12:41:11 +0200

    fix: added pipefail (#40)
    * fix: added pipefail
    
    * test: trying to timeout kubectl
    
    * test: adding fail flag
    
    * test: unchaining && blocksto test
    
    * test: removing -f flag
    
    * fix: removed debugging
    
    * chore: bumped action versions and replaced versions with sha

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # This script is used to install the tools required for the platform.
 
+# Fail script on any error
+set -euo pipefail
+
 # 01. Terraform
 echo "Installing Terraform..."
-curl https://releases.hashicorp.com/terraform/1.12.0/terraform_1.12.0_linux_amd64.zip -o terraform.zip && unzip terraform.zip && sudo mv terraform /usr/local/bin/terraform && rm terraform.zip
+curl https://releases.hashicorp.com/terraform/1.12.0/terraform_1.12.0_linux_amd64.zip -o terraform.zip
+unzip terraform.zip
+sudo mv terraform /usr/local/bin/terraform
+rm terraform.zip
 echo "Terraform installed successfully."
 
 # 02. Azure CLI
@@ -49,17 +55,20 @@ curl -fsSL https://raw.githubusercontent.com/databricks/setup-cli/v0.279.0/insta
 echo "Installing Bruno..."
 curl -sfL https://github.com/usebruno/bruno/releases/download/v2.3.0/bruno_2.3.0_amd64_linux.deb -o bruno.deb
 sudo apt install libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libsecret-1-0 -y
-sudo dpkg -i bruno.deb && rm bruno.deb && echo "Bruno installed successfully."
+sudo dpkg -i bruno.deb
+rm bruno.deb
+echo "Bruno installed successfully."
 
 # 05. gh cli
 echo "Installing gh cli..."
-(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
-	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
-	&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-	&& sudo apt update \
-	&& sudo apt install gh -y && echo "gh cli installed successfully."
+type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)
+sudo mkdir -p -m 755 /etc/apt/keyrings
+wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null
+sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh -y
+echo "gh cli installed successfully."
 
 # 06. ORAS
 echo "Installing oras..."
